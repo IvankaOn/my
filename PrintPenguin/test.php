8da7f16eb9a0cf82014c4d4b89ac5a7d7b537791
IvankaOn <ivanka.onofriychuk@gmail.com>
2025-03-11 22:49:25 +0100

    delete some files

--- PrintPenguin/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once __DIR__ . '/app/php/autoloader.php';
-	
-	
-	//	Cache::set('test_array', ['test' => 'array']);
-
-	//Cache::delete('testing2');
-	//print_r(Cache::get('testing2'));
